b6b36b0c2b57ea32e07db6fc764bb09d7ca14bbe
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2025-05-25 22:24:50 +0800

    static to dynamic pages realquick part 2 index.php

---
+++ sections/contact.php
@@ -0,0 +1,79 @@
+ <section id="contact" class="contact section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Contact</h2>
+        <p>
+          Feel free to reach out for any online projects or collaborations. The best way to contact me is via email, and
+          I'll respond as soon as possible.
+        </p>
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4">
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+              <i class="bi bi-geo-alt flex-shrink-0"></i>
+              <div>
+                <h3>Address</h3>
+                <p>205 NPC Mendez Street, Baesa, Quezon City</p>
+              </div>
+            </div><!-- End Info Item -->
+
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+              <i class="bi bi-telephone flex-shrink-0"></i>
+              <div>
+                <h3>Call Me</h3>
+                <p>+63 926 322 6163</p>
+              </div>
+            </div><!-- End Info Item -->
+
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+              <i class="bi bi-envelope flex-shrink-0"></i>
+              <div>
+                <h3>Email Me</h3>
+                <p>lancemagollado46@gmail.com</p>
+              </div>
+            </div><!-- End Info Item -->
+          </div>
+
+          <div class="col-lg-8">
+            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up"
+              data-aos-delay="200">
+              <div class="row gy-4">
+
+                <div class="col-md-6">
+                  <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
+                </div>
+
+                <div class="col-md-6 ">
+                  <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+
+                  <button type="submit">Send Message</button>
+                </div>
+
+              </div>
+            </form>
+          </div><!-- End Contact Form -->
+
+        </div>
+
+      </div>
+
+    </section>
---
+++ sections/resume.php
@@ -0,0 +1,88 @@
+  <section id="resume" class="resume section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Resume</h2>
+        <p>A dedicated <i>Information Technology student</i> with skills in <i>software development, database
+            management, and IoT applications</i>.
+          Experienced in <i>VB.NET, Java, MySQL, and Arduino</i>, with hands-on projects in <i>system development and
+            smart solutions</i>.
+          Also a <i>freelance academic tutor at StudyPool</i>, assisting students in programming and computer science.
+        </p>
+        </p>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Training</h3>
+
+            <div class="resume-item pb-0">
+              <h4>Training for Work Scholarship Program (TWSP)</h4>
+              <h5>November 11, 2024 - December 30, 2024</h5>
+              <p><em>Certified in Computer Programming (Java) - NC III</em></p>
+              <p>
+                Successfully completed the TESDA-accredited Training for Work Scholarship Program (TWSP)
+                specializing in Computer Programming (Java), enhancing skills in object-oriented programming,
+                software development, and database integration.
+              </p>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Bachelor of Science in Information Technology</h4>
+              <h5>2023 - Present</h5>
+              <p><em>Quezon City University, Quezon City, Metro Manila</em></p>
+              <p>Currently pursuing a Bachelor of Science in Information Technology, focusing on software development,
+                database management, and IoT applications.
+                Gaining hands-on experience in programming and emerging technologies to develop innovative solutions.
+              </p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>ICT with Specialization in Computer Programming</h4>
+              <h5>2019 - 2021</h5>
+              <p><em>Rosario Institute, Rosario, Cavite</em></p>
+              <p>Completed the Track-Information and Communications Technology (ICT) with a specialization in Computer
+                Programming,
+                gaining foundational knowledge in coding, software development, and database management.</p>
+            </div><!-- Edn Resume Item -->
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Freelance Academic Tutor & Contributor</h4>
+              <h5>2023 - Present</h5>
+              <p><em>StudyPool (Remote)</em></p>
+              <ul>
+                <li>Provided academic assistance to students by answering subject-specific questions.
+                </li>
+                <li>Specialized in topics related to programming, computer science, and technology-related subjects.
+                </li>
+                <li>Developed clear and well-structured explanations to help students improve their learning outcomes.
+                </li>
+              </ul>
+            </div>
+            <!-- <div class="resume-item">
+              <h4>Graphic design specialist</h4>
+              <h5>2017 - 2018</h5>
+              <p><em>Stepping Stone Advertising, New York, NY</em></p>
+              <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and
+                  advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              </ul>
+            </div>Edn Resume Item -->
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
