d81898a127b4bfaf5887eb536e7291650f711449
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2025-06-16 22:49:30 +0800

    final sectioning in index.php,skills.php and about.php

--- index.php
+++ index.php
@@ -55,7 +55,11 @@
      ?>
     <!-- /About Section -->
 
-
+    <!-- Skills Section -->
+    <?php
+    include_once("sections/skills.php");
+    ?>
+    <!-- /Skills Section -->
     <!-- Stats Section -->
     <?php
     // include_once("sections/stats.php");
