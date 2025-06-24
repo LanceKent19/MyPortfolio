6f19b2061690ac53faf332664568f9538bd013f9
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2025-06-24 23:49:46 +0800

    initial pre-loader design

--- index.php
+++ index.php
@@ -115,7 +115,25 @@
     ?>
 
   <!-- Preloader -->
-  <div id="preloader"></div>
+  <div id="preloader">
+    <div class="preloader-inner">
+      <div class="preloader-progress-wrap">
+        <svg class="preloader-progress" width="90" height="90" viewBox="0 0 90 90">
+          <defs>
+            <linearGradient id="preloader-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" stop-color="#1e3c72"/>
+              <stop offset="50%" stop-color="#f46b45"/>
+              <stop offset="100%" stop-color="#eea849"/>
+            </linearGradient>
+          </defs>
+          <circle class="preloader-bg" cx="45" cy="45" r="40" stroke-width="8" fill="none" />
+          <circle class="preloader-bar" cx="45" cy="45" r="40" stroke-width="8" fill="none" stroke="url(#preloader-gradient)" />
+        </svg>
+        <img src="assets/img/Logo-modified.png" alt="Logo" class="preloader-logo swing-effect">
+      </div>
+      <div class="preloader-title">BuiltByLance</div>
+    </div>
+  </div>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
