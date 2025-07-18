975db5ca5f38210f5a7c37ea1c3dc4ea033648b1
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2025-07-18 09:50:04 +0800

    part 2 of initial navbar

--- includes/header.php
+++ includes/header.php
@@ -4,18 +4,16 @@
     <div class="profile-img">
       <img src="assets/img/me-photo/Profile-Pic.png" alt="gehe" class="img-fluid rounded-circle">
     </div>
-
-    <a href="index.php" class="logo d-flex align-items-center justify-content-center">
-      <!-- Uncomment the line below if you also wish to use an image logo -->
-      <h1 class="sitename">Magollado, Lance Kent Geoffrey B.</h1>
-    </a>
-    <h6 class="sitename">Software Developer Student</h6>
+    <div class="portfolio-name logo d-flex">
+    <h1>Magollado, Lance Kent Geoffrey B.</h1>
+    <h6>Software Developer Student</h6>
+    </div>
     <nav id="navmenu" class="navmenu">
       <ul>
         <li><a href="#hero" class="active"><i class="bi bi-house navicon"></i>Home</a></li>
         <li><a href="#about"><i class="bi bi-person navicon"></i> About</a></li>
         <li><a href="#resume"><i class="bi bi-file-earmark-text navicon"></i> Resume</a></li>
-        <li><a href="#portfolio"><i class="bi bi-folder navicon"></i> Projects</a></li>
+        <li><a href="#portfolio"><i class="bi bi-folder navicon"></i>Projects</a></li>
       </ul>
     </nav>
 
