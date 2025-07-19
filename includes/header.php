05df27fe2c6d9d2f368347e04428ac829d481b38
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2025-07-19 20:40:45 +0800

    modifying the about and index.php in a single page

--- includes/header.php
+++ includes/header.php
@@ -2,19 +2,19 @@
     <i class="header-toggle d-xl-none bi bi-list"></i>
 
     <div class="profile-img">
-      <img src="assets/img/me-photo/Profile-Pic.png" alt="gehe" class="img-fluid rounded-circle">
+      <img src="../assets/img/me-photo/Profile-Pic.png" alt="gehe" class="img-fluid rounded-circle">
     </div>
     <div class="portfolio-name logo d-flex">
     <h1>Magollado, Lance Kent Geoffrey B.</h1>
     <h6>Software Developer</h6>
     </div>
     <div class="resume-btn">
-        <a href="assets/files/cv.pdf" download><i class="bi bi-download navicon"></i>Resume</a>
+        <a href="../assets/files/cv.pdf" download><i class="bi bi-download navicon"></i>Resume</a>
     </div>
     <nav id="navmenu" class="navmenu">
       <ul>
-        <li><a href="#hero" class="active"><i class="bi bi-house navicon"></i>Home</a></li>
-        <li><a href="#about"><i class="bi bi-person navicon"></i> About</a></li>
+        <li><a href="../sections/index.php" class="active"><i class="bi bi-house navicon"></i>Home</a></li>
+        <li><a href="../sections/about.php"><i class="bi bi-person navicon"></i> About</a></li>
         <li><a href="#resume"><i class="bi bi-file-earmark-text navicon"></i> Resume</a></li>
         <li><a href="#portfolio"><i class="bi bi-folder navicon"></i>Projects</a></li>
         <li><a href="#"><i class="bi bi-laptop navicon"></i>Hackathons</a></li>  
